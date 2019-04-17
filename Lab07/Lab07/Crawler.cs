84ed64664df1cdab2522ee439518e4eb9f16c2e4
Alican <alican.dogan@deu.edu.tr>
2019-04-17 10:28:43 +0300

    cozum eklendi

---
+++ Lab07/Lab07/Crawler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Lab07
+{
+    class Crawler
+    {
+        private string html;
+        private string url;
+
+        public Crawler(string url)
+        {
+            this.url = url;
+            html = new WebClient().DownloadString(this.url);
+        }
+
+        public ArrayList FindInWebSite(String regex)
+        {
+            ArrayList list = new ArrayList();
+            Regex ItemRegex = new Regex(regex, RegexOptions.Compiled);
+            if (ItemRegex.Matches(html).Count > 0)
+            {
+                foreach (Match ItemMatch in ItemRegex.Matches(html))
+                {
+                    list.Add(ItemMatch.Value);
+                }
+            }
+            else
+            {
+                throw new RegexNotFoundException($"Aranan string ifadesi {url}' sitesinde bulunamadı.");
+            }
+
+            return list;
+        }  
+      
+       
+    }
+}
---
+++ Lab07/Lab07/RegexNotFoundException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab07
+{
+    class RegexNotFoundException : Exception
+    {
+        public RegexNotFoundException()
+        {
+        }
+
+        public RegexNotFoundException(string message)
+        : base(message)
+        {
+        }
+
+        public RegexNotFoundException(string message, Exception inner)
+        : base(message, inner)
+        {
+        }
+    }
+}
